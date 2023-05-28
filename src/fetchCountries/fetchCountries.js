868d83a6eea427bd24d91f879d25481da6dcc972
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-05-28 14:08:55 +0200

    +

--- src/fetchCountries/fetchCountries.js
+++ src/fetchCountries/fetchCountries.js
@@ -5,23 +5,8 @@
  */
 
 function fetchCountries(name = "") {
-    // const fields = 'name,capital,population,flags.svg,languages'
-
-    // const params = new URLSearchParams({
-    //     name: name,
-    //     capital: capital,
-    //     population: population,
-    //     flag: flags.svg,
-    //     languages: languages.name,
-    //     // languages: languages.map((language) => language.name),
-    // })
-    // const params = new URLSearchParams({ name, capital, population, flags: { svg }, languages: { name }})
 
     return fetch(`https://restcountries.com/v2/name/${name}`)
-
-    // return fetch(`https://restcountries.com/v2/name/${name}?fields=${fields}`)
-    // return fetch(`https://restcountries.com/v2/name/${name}?fields=${params}`)
-    // return fetch(`https://restcountries.com/v2/name/${name}?fields={name};{capital};{population};{flags.svg};{languages.name};`)
 
     .then(response => {
         console.log(response);
